32846fcb4c40f624914871fbedb0f8cf41ed4fe9
zhao.feng <zhao.feng@Ctrip.com>
2018-01-31 17:04:27 +0800

    简单分布式爬虫

--- SpiderServer.py
+++ SpiderServer.py
@@ -1,6 +1,7 @@
 # encoding:utf-8
-
-import Queue
+# multiprocessing.Queue 是跨进程通信队列
+# Queue.Queue 进程内非阻塞队列,各自私有 无法跨进程通信
+from multiprocessing import queues
 from multiprocessing.managers import BaseManager
 import UrlManager
 import DataSave
@@ -10,73 +11,71 @@ from multiprocessing.process import Process
 class SpiderServer(object):
 
     def __init__(self):
-        # url管理器把url给爬虫节点的通道
-        self.urlQueue = Queue.Queue()
-        # 爬虫节点把数据返回解析通道
-        self.resultQueue = Queue.Queue()
-        # 数据解析通道
-        self.parseQueue = Queue.Queue()
-        # 存储数据通道
-        self.storeQueue = Queue.Queue()
 
-    def start_server(self):
+        print "spider server init"
+
+    def start_server(self, urlQueue, resultQueue):
         """
         
         :rtype: BaseManager 
         """
         # 把 url 任务队列和结果队列在网络中暴露
-        BaseManager.register('get_task_queue', callable=lambda: self.urlQueue)
-        BaseManager.register('get_result_queue', callable=lambda: self.resultQueue)
+        BaseManager.register('get_task_queue', callable=lambda: urlQueue)
+        BaseManager.register('get_result_queue', callable=lambda: resultQueue)
 
         # 绑定端口 设置验证口令
-        manager = BaseManager(address=('', 8001), authkey='ZF')
+        manager = BaseManager(address=('', 8101), authkey='ZF')
         return manager
 
-    def url_manager_proc(self, root_url):
+    def url_manager_proc(self, root_url, urlQueue, parseQueue):
         url_manager = UrlManager.UrlManager()
         url_manager.add_new_urls(root_url)
         while True:
-            while(url_manager.has_new_url()):
+            if(url_manager.has_new_url()):
                 new_url = url_manager.get_new_url()
-                self.urlQueue.put(new_url)
-                if url_manager.get_old_url_size() > 2000:
-                    self.urlQueue.put('END')
-                    return
+                urlQueue.put(new_url)
+            else:
+                urlQueue.put('END')
+
 
             try:
-                if not self.parseQueue.empty():
+                if not parseQueue.empty():
                     # 获取解析出来的新的urls
-                    urls = self.parseQueue.get()
+                    urls = parseQueue.get()
                     url_manager.add_new_urls(urls)
-            except BaseException, e:
-                time.sleep(0.1)
-
-
-    def result_proc(self):
-        while True:
-            try:
-                if not self.resultQueue.empty():
-                    content = self.resultQueue.get(True)
-                    print  "content: %s" % content
-                    if content['new_urls'] == 'END':
-                        print "接收到通知结束爬虫"
-                        self.storeQueue.put("END")
-                        return
-
-                    self.parseQueue.put(content['new_urls'])
-                    self.storeQueue.put(content['new_data'])
                 else:
                     time.sleep(0.1)
             except BaseException, e:
                 time.sleep(0.1)
 
 
-    def store_proc(self):
+    def result_proc(self, resultQueue, storeQueue):
+        while True:
+            print "result_proc %s" % resultQueue.empty()
+            try:
+                if not resultQueue.empty():
+                    content = resultQueue.get(True)
+                    print  "content: %s" % content
+                    if content['new_urls'] == 'END':
+                        print "接收到通知结束爬虫"
+                        storeQueue.put("END")
+                        return
+
+                    parseQueue.put(content['new_urls'])
+                    storeQueue.put(content['new_data'])
+                else:
+                    time.sleep(0.1)
+            except BaseException, e:
+                time.sleep(0.1)
+
+
+    def store_proc(self, storeQueue):
         data_save = DataSave.DataSave()
         while True:
+            print "store_proc %s" % storeQueue.empty()
             try:
-                if not self.storeQueue.empty():
-                    data = self.storeQueue.get()
+                if not storeQueue.empty():
+                    data = storeQueue.get()
                     if data == "END":
                         print "存储进程结束"
                         data_save.output_data()
@@ -90,17 +89,30 @@ class SpiderServer(object):
 
 
 
+
 if __name__ == '__main__':
+    # url管理器把url给爬虫节点的通道
+    urlQueue = queues.Queue()
+    #print urlQueue
+    # 爬虫节点把数据返回解析通道
+    resultQueue = queues.Queue()
+    # 数据解析通道
+    parseQueue = queues.Queue()
+    # 存储数据通道
+    storeQueue = queues.Queue()
     server = SpiderServer()
-    server_manager = server.start_server()
-    url_manager_proc = Process(target=server.url_manager_proc, args=('http://e.pingan.com/pa18shoplife/category/list.jsp',))
-    result_proc = Process(target=server.result_proc)
-    store_proc = Process(target=server.store_proc)
+    server_manager = server.start_server(urlQueue, resultQueue)
+    url_manager_proc = Process(target=server.url_manager_proc, args=('http://e.pingan.com/pa18shoplife/category/list.jsp', urlQueue, parseQueue))
+    #print urlQueue
+    result_proc = Process(target=server.result_proc, args=(resultQueue, storeQueue))
+    store_proc = Process(target=server.store_proc, args=(storeQueue,))
     url_manager_proc.start()
     result_proc.start()
     store_proc.start()
 
     server_manager.get_server().serve_forever()
+
+
 
 
 
--- SpiderWork.py
+++ SpiderWork.py
@@ -4,29 +4,57 @@ from multiprocessing.managers import BaseManager
 from HtmlDownloader import HtmlDownload
 from HtmlParse import HtmlParse
 
+
 class SpiderWork(object):
 
     def __init__(self):
+        print "spider work init start"
 
         BaseManager.register("get_task_queue")
         BaseManager.register("get_result_queue")
 
         server_addr = '127.0.0.1'
-        self.manager = BaseManager(address=(server_addr, 8001), authkey='ZF')
-        self.manager.connect()
+        self.manager = BaseManager(address=(server_addr, 8101), authkey='ZF')
+        try:
+            self.manager.connect()
+        except BaseException, e:
+            print e
 
-        self.task = self.manager.get_task_queue()
-        self.result = self.manager.get_result_queue()
+
+
+        self.task = self.manager.get_task_queue()  # type: Queue
+
+        print self.task, self.task.empty()
+
+        self.result = self.manager.get_result_queue() # type: Queue
         self.downloader = HtmlDownload()
         self.parser = HtmlParse()
+        print "spider work init finish"
 
 
     def crawl(self):
-        pass
+        while True:
+            try:
+                if not self.task.empty():
+                    new_url = self.task.get()
+
+                    if new_url == "END":
+                        self.result.put({'new_urls':"END","new_data":"END"})
+                        return
+                    cont = self.downloader.download_get(new_url)
+                    new_urls, new_data = self.parser.do_parser(new_url, cont)
+                    self.result.put({'new_urls':new_urls,'new_data':new_data})
+            except EOFError, e:
+                print "连接工作节点失败"
+                return
+            except Exception, e:
+                print e
+                print 'Crawl fail work'
 
 
 if __name__ == '__main__':
-    pass
+    spider_work = SpiderWork()
+    spider_work.crawl()
 
 
 
